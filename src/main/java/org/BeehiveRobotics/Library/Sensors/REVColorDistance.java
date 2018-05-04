83df944289f7893a362304b82c4d3b5fdcafc40c
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-05-04 16:06:47 -0600

    fixed variable names

--- src/main/java/org/BeehiveRobotics/Library/Sensors/REVColorDistance.java
+++ src/main/java/org/BeehiveRobotics/Library/Sensors/REVColorDistance.java
@@ -1,5 +1,6 @@
 package org.BeehiveRobotics.Library.Sensors;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -7,40 +8,40 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class REVColorDistance {
-	private ColorSensor csen;
-	private DistanceSensor dsen;
+	private ColorSensor colorSensor;
+	private DistanceSensor distanceSensor;
 
-	public REVColorDistance(OpMode opMode, String colorName, String distanceName) {
-		HardwareMap hw_map = opMode.hardwareMap;
-		csen = hw_map.get(ColorSensor.class, colorName);
-		dsen = hw_map.get(DistanceSensor.class, distanceName);
+	public REVColorDistance(LinearOpMode opMode, String colorName, String distanceName) {
+		HardwareMap hardwareMap = opMode.hardwareMap;
+		colorSensor = hardwareMap.get(ColorSensor.class, colorName);
+		distanceSensor = hardwareMap.get(DistanceSensor.class, distanceName);
 	}
 
 	public ColorSensor getColorSensor() {
-		return csen;
+		return colorSensor;
 	}
 	public DistanceSensor getDistanceSensor() {
-		return dsen;
+		return distanceSensor;
 	}
 
-	public double getDistance(DistanceUnit dunit) {
-		return dsen.getDistance(dunit);
+	public double getDistance(DistanceUnit distanceUnit) {
+		return distanceSensor.getDistance(distanceUnit);
 	}
 
 	public double getRed() {
-		return csen.red();
+		return colorSensor.red();
 	}
 
 	public double getBlue() {
-		return csen.blue();
+		return colorSensor.blue();
 	}
 	
 	public double getGreen() {
-		return csen.green();
+		return colorSensor.green();
 	}
 	
 	public double getAlpha() {
-		return csen.alpha();
+		return colorSensor.alpha();
 	}
 
 }
