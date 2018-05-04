f32c813d9a2d777ff32899c0a503fe578cab4aef
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-05-04 15:18:06 -0600

    made a gamepad wrapper to invert y axis

---
+++ src/main/java/org/BeehiveRobotics/Library/Util/Controller.java
@@ -0,0 +1,75 @@
+package org.BeehiveRobotics.Library.Util;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Controller {
+    private Gamepad gamepad;
+
+    public Controller(Gamepad gamepad) {
+        this.gamepad = gamepad;
+    }
+
+    public double leftStickX() {
+        return gamepad.left_stick_x;
+    }
+
+    public double leftStickY() {
+        return -gamepad.left_stick_y;
+    }
+
+    public double rightStickX() {
+        return gamepad.right_stick_x;
+    }
+
+    public double rightStickY() {
+        return -gamepad.right_stick_y;
+    }
+
+    public double leftTrigger() {
+        return gamepad.left_trigger;
+    }
+
+    public double rightTrigger() {
+        return gamepad.right_trigger;
+    }
+
+    public boolean leftBumper() {
+        return gamepad.left_bumper;
+    }
+
+    public boolean rightBumper() {
+        return gamepad.right_bumper;
+    }
+
+    public boolean dpad_up() {
+        return gamepad.dpad_up;
+    }
+
+    public boolean dpad_down() {
+        return gamepad.dpad_down;
+    }
+
+    public boolean dpad_left() {
+        return gamepad.dpad_left;
+    }
+
+    public boolean dpad_right() {
+        return gamepad.dpad_right;
+    }
+
+    public boolean a() {
+        return gamepad.a;
+    }
+
+    public boolean b() {
+        return gamepad.b;
+    }
+
+    public boolean x() {
+        return gamepad.x;
+    }
+
+    public boolean y() {
+        return gamepad.y;
+    }
+}
