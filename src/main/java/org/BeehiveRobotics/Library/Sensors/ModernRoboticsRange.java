e2028d81d845e083866671ed60b0dc76b45d29da
ohninedotseven <harrison@malw.io>
2018-05-04 18:58:36 -0600

    Add Modern Robotics range sensor

---
+++ src/main/java/org/BeehiveRobotics/Library/Sensors/ModernRoboticsRange.java
@@ -0,0 +1,44 @@
+package org.BeehiveRobotics.Library.Sensors;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ModernRoboticsRange {
+	private ModernRoboticsI2cRangeSensor rangeSensor;
+	private DistanceUnit units;
+
+	public ModernRoboticsRange(LinearOpMode opMode, String rangeName) {
+		//units in this will automatically default to cm
+		units = DistanceUnit.cm;
+		HardwareMap hwMap = opMode.hardwareMap;
+		rangeSensor = hwMap.get(ModernRoboticsI2cRangeSensor.class, rangeName);
+	}
+
+	public ModernRoboticsRange setUnit(DistanceUnit unit) {
+		units = unit;
+		return this;
+	}
+
+	public double getRawUltrasonic() {
+		return rangeSensor.rawUltrasonic();
+	}
+
+	public double getRawOptical() {
+		return rangeSensor.rawOptical();
+	}
+	
+	public double getCMOptical() {
+		return rangeSensor.cmOptical();
+	}
+
+	public double getDistance() { //this will get the distance in whatever units you specify lol
+		return rangeSensor.getDistance(units);
+	}
+	
+	public ModernRoboticsI2cRangeSensor getSensor() {
+		return this.rangeSensor;
+	}
+
+
+}
