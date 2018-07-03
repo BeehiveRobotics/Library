50a668d20a381e26d72ee3e75092280fbd6b57d0
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-07-03 17:39:32 -0600

    starting servo speed managing (multi-threaded)

--- src/main/java/org/BeehiveRobotics/Library/Servos/Servo.kt
+++ src/main/java/org/BeehiveRobotics/Library/Servos/Servo.kt
@@ -2,17 +2,51 @@ package org.BeehiveRobotics.Library.Servos
 
 import org.BeehiveRobotics.Library.Util.BROpMode
 import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.util.Range
 
 class Servo(private val opMode: BROpMode, val name: String): Runnable {
+
     private val servo: Servo = opMode.hardwareMap.get(Servo::class.java, name)
+    private var servoPosition: Double = 0.0
+    private var servoSpeed: Double = 1.0
+    private var rangeOfMotion: Double = 180.0
+    private var thread: Thread = Thread(this)
+
+    init {
+        thread.start()
+    }
+
+    fun setRange(degrees: Double) {
+        this.rangeOfMotion = degrees
+    }
+    
     fun setPosition(position: Double) {
+        this.servoPosition = position
         servo.setPosition(position)
     }
+    
+    fun setSpeed(speed: Double) {
+        this.servoSpeed = Math.abs(Range.clip(Math.abs(speed), 0.0, 1.0))
+    }
+
     fun setDirection(direction: Servo.Direction): org.BeehiveRobotics.Library.Servos.Servo {
         servo.setDirection(direction)
         return this
     }
+    
+    fun getPosition(): Double {
+        return servo.position
+    }
+    
     override fun run() {
+        var previousTarget = this.servoPosition
+        while(opMode.opModeIsActive()) {
+            if(previousTarget != this.servoPosition) {
+                val distance = getPosition() - this.servoPosition
 
+                previousTarget = this.servoPosition
+            }
+
+        }
     }
 }
---
+++ src/main/java/org/BeehiveRobotics/Library/Servos/ServoModel.kt
@@ -0,0 +1,5 @@
+package org.BeehiveRobotics.Library.Servos
+
+enum class ServoModel(val RPM: Double) {
+    REV(60.0) // TODO: TEST THIS NUMBER. THIS IS JUST A FILLER FOR NOW
+}
