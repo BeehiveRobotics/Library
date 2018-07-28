325ca604abeaa669cd09cc9bff7d691e01391c39
KadenGordon <kaden.m.gordon@gmail.com>
2018-07-27 23:10:28 -0600

    Servo speed is probably close to right. Also it is kotlinized

--- src/main/java/org/BeehiveRobotics/Library/Servos/Servo.kt
+++ src/main/java/org/BeehiveRobotics/Library/Servos/Servo.kt
@@ -4,56 +4,50 @@ import org.BeehiveRobotics.Library.Util.BROpMode
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.util.Range
 import org.BeehiveRobotics.Library.Systems.RobotSystem
+import com.qualcomm.robotcore.util.ElapsedTime
 
-class Servo(private val opMode: BROpMode, val name: String): RobotSystem(opMode), Runnable {
-
-    private val servo: Servo = opMode.hardwareMap.get(Servo::class.java, name)
-    private var servoPosition: Double = 0.0
-    private var servoSpeed: Double = 1.0
-    private var rangeOfMotion: Double = 180.0
-    private var thread: Thread = Thread(this)
-
+class Servo(private val opMode: BROpMode, val name: String, var targetPosition: Double = 0.0): RobotSystem(opMode), Runnable {
     enum class ServoModel(val RPM: Double) {
         REV(60.0) // TODO: TEST THIS NUMBER. THIS IS JUST A FILLER FOR NOW
     }
+    private val servo: Servo = opMode.hardwareMap.get(Servo::class.java, name)
+    var model: ServoModel = ServoModel.REV
+    var MIN_POSITION = 0.0
+    var MAX_POSITION = 1.0
+    var position: Double
+        set(value) {
+            servo.position = Range.clip(value, MIN_POSITION, MAX_POSITION)
+        }
+        get() = servo.position
+    var servoSpeed: Double = 1.0
+        set(value) {
+            Range.clip(Math.abs(value), 0.0, 1.0)
+        }
+    var degrees: Double = 180.0 // the degrees it can move (not from 0.0 to 1.0, from whatever limits the user sets)
+    private val thread: Thread = Thread(this)
+    var direction // as in reverse or forward
+        set(value) {
+            servo.direction = value
+        }
+        get() = servo.direction
 
     init {
-        thread.start()
-    }
-
-    fun setDirection(direction: com.qualcomm.robotcore.hardware.Servo.Direction): org.BeehiveRobotics.Library.Servos.Servo {
-        servo.setDirection(direction)
-        return this
-    }
-
-    fun setSpeed(speed: Double): org.BeehiveRobotics.Library.Servos.Servo {
-        this.servoSpeed = Range.clip(Math.abs(speed), 0.0, 1.0)
-        return this
-    }
-
-    fun setRange(degrees: Double): org.BeehiveRobotics.Library.Servos.Servo {
-        this.rangeOfMotion = degrees
-        return this
-    }
-    
-    fun setPosition(position: Double) {
-        this.servoPosition = position
-    }
-    
-    fun getPosition(): Double {
-        return servo.position
-    }
-    
+        thread.start() // it can just constantly run a thread, why not
+    }    
     override fun run() {
-        var previousTarget = this.servoPosition
+        val time = ElapsedTime()
+        var index = 1
+        val updateSpeed = 0.1 //this many seconds, it updates
+        val increment = 1000 * updateSpeed //how many milliseconds each update takes
         while(opMode.opModeIsActive()) {
-            if(previousTarget != this.servoPosition) {
-                val distance = getPosition() - this.servoPosition
-
-                previousTarget = this.servoPosition
-            }
-
+            val RPS = model.RPM/60 //rotations per second
+            val DPS = RPS*360 //degrees per second
+            val DPI = DPS/increment //degreees per increment
+            val PPI = DPI/degrees //proportion per increment
+            val CPPI = PPI*servoSpeed //calculated ppi, taking into account speed
+            position = if(targetPosition<position) position - CPPI else if(targetPosition>position) position + CPPI else position
+            sleep(((index * increment) - time.milliseconds()).toLong())
+            index.inc()
         }
-    }
+    } //Test this speed control
 }
-//TODO: Finish this with speed control and more Kotlinization, kinda like ../Motors/Motor.kt has
