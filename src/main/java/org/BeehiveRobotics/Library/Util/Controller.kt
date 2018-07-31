010ba5486bb6650b05e823eb42a72182fb62f24d
KadenGordon <kaden.m.gordon@gmail.com>
2018-07-31 17:52:56 -0600

    Made Controller more like Gamepad, so you don't have to invoke functions

--- src/main/java/org/BeehiveRobotics/Library/Util/Controller.kt
+++ src/main/java/org/BeehiveRobotics/Library/Util/Controller.kt
@@ -2,7 +2,7 @@ package org.BeehiveRobotics.Library.Util
 
 import com.qualcomm.robotcore.hardware.Gamepad
 
-class Controller(val gamepad: Gamepad) {
+class Controller(private val gamepad: Gamepad) {
     private var aPrev = false
     private var bPrev = false
     private var xPrev = false
@@ -13,53 +13,105 @@ class Controller(val gamepad: Gamepad) {
     private var dpad_downPrev = false
     private var dpad_leftPrev = false
     private var dpad_rightPrev = false
+    
+    var leftStickX = 0.0
+        private set
+        get() = gamepad.left_stick_x.toDouble()
+    var leftStickY = 0.0
+        private set
+        get() = -gamepad.left_stick_y.toDouble()
+    var rightStickX = 0.0
+        private set
+        get() = gamepad.right_stick_x.toDouble()
+    var rightStickY = 0.0
+        private set
+        get() = -gamepad.right_stick_y.toDouble()
 
-    fun leftStickX(): Double = gamepad.left_stick_x.toDouble()
-    fun leftStickY(): Double = -gamepad.left_stick_y.toDouble()
-    fun rightStickX(): Double = gamepad.right_stick_x.toDouble()
-    fun rightStickY(): Double = -gamepad.right_stick_y.toDouble()
+    var leftTrigger = 0.0
+        private set
+        get() = gamepad.left_trigger.toDouble()
+    var rightTrigger = 0.0
+        private set
+        get() = gamepad.right_trigger.toDouble()
 
-    fun leftTrigger(): Double = gamepad.left_trigger.toDouble()
-    fun rightTrigger(): Double = gamepad.right_trigger.toDouble()
+    var leftBumper = false
+        private set
+        get() = gamepad.left_bumper
+    var rightBumper = false
+        private set
+        get() = gamepad.right_bumper
 
-    fun leftBumper(): Boolean = gamepad.left_bumper
-    fun rightBumper(): Boolean = gamepad.right_bumper
+    var dpad_up = false
+        private set
+        get() = gamepad.dpad_up
+    var dpad_down = false
+        private set
+        get() = gamepad.dpad_down
+    var dpad_left = false
+        private set
+        get() = gamepad.dpad_left
+    var dpad_right = false
+        private set
+        get() = gamepad.dpad_right
 
-    fun dpad_up(): Boolean = gamepad.dpad_up
-    fun dpad_down(): Boolean = gamepad.dpad_down
-    fun dpad_left(): Boolean = gamepad.dpad_left
-    fun dpad_right(): Boolean = gamepad.dpad_right
+    var a = false
+        private set
+        get() = gamepad.a
+    var b = false
+        private set
+        get() = gamepad.b
+    var x = false
+        private set
+        get() = gamepad.x
+    var y = false
+        private set
+        get() = gamepad.y
 
-    fun a(): Boolean = gamepad.a
-    fun b(): Boolean = gamepad.b
-    fun x(): Boolean = gamepad.x
-    fun y(): Boolean = gamepad.y
+    var aToggle = false
+        private set
+        get() = a && !aPrev
+    var bToggle = false
+        private set
+        get() = b && !bPrev
+    var xToggle = false
+        private set
+        get() = x && !xPrev
+    var yToggle = false
+        private set
+        get() = y && !yPrev
 
-    fun aToggle(): Boolean = a() && !aPrev
-    fun bToggle(): Boolean = b() && !bPrev
-    fun xToggle(): Boolean = x() && !xPrev
-    fun yToggle(): Boolean = y() && !yPrev
+    var leftBumperToggle = false
+        private set
+        get() = leftBumper && !leftBumperPrev
+    var rightBumperToggle = false
+        private set
+        get() = rightBumper && !rightBumperPrev
 
-    fun leftBumperToggle(): Boolean = leftBumper() && !leftBumperPrev
-    fun rightBumperToggle(): Boolean = rightBumper() && !rightBumperPrev
-
-    fun dpad_upToggle(): Boolean = dpad_up() && !dpad_upPrev
-    fun dpad_downToggle(): Boolean = dpad_down() && !dpad_downPrev
-    fun dpad_leftToggle(): Boolean = dpad_left() && !dpad_leftPrev
-    fun dpad_rightToggle(): Boolean = dpad_right() && !dpad_rightPrev
+    var dpad_upToggle = false
+        private set
+        get() = dpad_up && !dpad_upPrev
+    var dpad_downToggle = false
+        private set
+        get() = dpad_down && !dpad_downPrev
+    var dpad_leftToggle = false
+        private set
+        get() = dpad_left && !dpad_leftPrev
+    var dpad_rightToggle = false
+        private set
+        get() = dpad_right && !dpad_rightPrev
 
     fun update() {
-        aPrev = a()
-        bPrev = b()
-        xPrev = x()
-        yPrev = y()
+        aPrev = a
+        bPrev = b
+        xPrev = x
+        yPrev = y
 
-        leftBumperPrev = leftBumper()
-        rightBumperPrev = rightBumper()
+        leftBumperPrev = leftBumper
+        rightBumperPrev = rightBumper
 
-        dpad_upPrev = dpad_up()
-        dpad_downPrev = dpad_down()
-        dpad_leftPrev = dpad_left()
-        dpad_rightPrev = dpad_right()
+        dpad_upPrev = dpad_up
+        dpad_downPrev = dpad_down
+        dpad_leftPrev = dpad_left
+        dpad_rightPrev = dpad_right
     }
 }
