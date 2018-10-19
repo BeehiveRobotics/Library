79e93eb47a72bfceeb6c827b01f3291488b52db2
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-10-19 17:36:59 -0600

    Added gearing option for the sample MecanumRobot TankRobots.

--- src/main/kotlin/org/BeehiveRobotics/Library/Robots/MecanumRobot.kt
+++ src/main/kotlin/org/BeehiveRobotics/Library/Robots/MecanumRobot.kt
@@ -2,11 +2,12 @@ package org.BeehiveRobotics.Library.Robots
 
 import org.BeehiveRobotics.Library.Util.BROpMode
 import org.BeehiveRobotics.Library.Systems.MecanumDrive
+import org.BeehiveRobotics.Library.Systems.DriveMotorSystem
 
-class MecanumRobot(private val opMode: BROpMode): Robot(opMode) {
+class MecanumRobot(private val opMode: BROpMode, private val gearedType: DriveMotorSystem.GearedType = DriveMotorSystem.GearedType.NORMAL): Robot(opMode) {
     lateinit var drive: MecanumDrive
     override fun init() {
-        drive = MecanumDrive(opMode)
+        drive = MecanumDrive(opMode, gearedType)
         drive.init()
     }
     override fun waitUntilNotBusy() {
--- src/main/kotlin/org/BeehiveRobotics/Library/Robots/TankRobot.kt
+++ src/main/kotlin/org/BeehiveRobotics/Library/Robots/TankRobot.kt
@@ -2,11 +2,12 @@ package org.BeehiveRobotics.Library.Robots
 
 import org.BeehiveRobotics.Library.Systems.TankDrive
 import org.BeehiveRobotics.Library.Util.BROpMode
+import org.BeehiveRobotics.Library.Systems.DriveMotorSystem
 
-class TankRobot(private val opMode: BROpMode): Robot(opMode) {
+class TankRobot(private val opMode: BROpMode, private val gearedType: DriveMotorSystem.GearedType = DriveMotorSystem.GearedType.NORMAL): Robot(opMode) {
     lateinit var drive: TankDrive
     override fun init() {
-        drive = TankDrive(opMode)
+        drive = TankDrive(opMode, gearedType)
         drive.init()
     }
     override fun waitUntilNotBusy() {
