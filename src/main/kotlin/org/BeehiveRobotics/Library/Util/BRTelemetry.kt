5abf4b06ce2711d92293ca67c324368d9464fe7a
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-12-08 10:17:26 -0700

    Redo BRTelemetry to be simpler and more like the original

--- src/main/kotlin/org/BeehiveRobotics/Library/Util/BRTelemetry.kt
+++ src/main/kotlin/org/BeehiveRobotics/Library/Util/BRTelemetry.kt
@@ -2,36 +2,20 @@ package org.BeehiveRobotics.Library.Util
 
 import com.qualcomm.robotcore.util.ElapsedTime
 
-class BRTelemetry(private val opMode: BROpMode): Runnable {
-    var hasNew = false
-    fun addLine(line: String, showImmediately: Boolean = false) {
-        hasNew = true
+class BRTelemetry(private val opMode: BROpMode) {
+    fun addLine(line: String) {
         opMode.telemetry.addLine(line)
-        if(showImmediately) update()
     }
-    fun addData(title: String, value: Any, showImmediately: Boolean = false) {
-        hasNew = true
+    fun showLine(line: String) {
+        addLine(line)
+        update()
+    }
+    fun addData(title: String, value: Any) {
         opMode.telemetry.addData(title, value)
-        if(showImmediately) update()
     }
-    fun update() {
-        opMode.telemetry.update()
-        hasNew = false
+    fun showData(title: String, value: Any) {
+        addData(title, value)
+        update()
     }
-    init {
-        val thread = Thread(this)
-        thread.start()
-    }
-    override fun run() {
-        while(!opMode.opModeIsActive()) {}
-        val time = ElapsedTime()
-        var i = 0
-        while(opMode.opModeIsActive()) {
-            while(time.milliseconds() < i * 250) {}
-            if(hasNew) update()
-            else time.reset()
-            i.inc()
-            if(i == Integer.MAX_VALUE) i = 0
-        }
-    }
+    fun update() = opMode.telemetry.update()
 }
