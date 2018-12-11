f087699f664231e6a217054ebd3f47908a773085
Kaden Gordon <Kaden.m.gordon@gmail.com>
2018-12-10 20:18:44 -0700

    Toggles are working super fast now

--- src/main/kotlin/org/BeehiveRobotics/Library/Util/Controller.kt
+++ src/main/kotlin/org/BeehiveRobotics/Library/Util/Controller.kt
@@ -13,7 +13,19 @@ class Controller(private val gamepad: Gamepad) {
     private var dpadDownPrev = false
     private var dpadLeftPrev = false
     private var dpadRightPrev = false
-    
+
+    private var DEBOUNCING_NUMBER_OF_SAMPLES = 20
+    private var aCountOn = 0
+    private var bCountOn = 0
+    private var xCountOn = 0
+    private var yCountOn = 0
+    private var leftBumperCountOn = 0
+    private var rightBumperCountOn = 0
+    private var dpadUpCountOn = 0
+    private var dpadDownCountOn = 0
+    private var dpadLeftCountOn = 0
+    private var dpadRightCountOn = 0
+
     var leftStickX = 0.0
         private set
         get() = gamepad.left_stick_x.toDouble()
@@ -69,38 +81,63 @@ class Controller(private val gamepad: Gamepad) {
 
     var aToggle = false
         private set
-        get() = a && !aPrev
+        get() = aCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var bToggle = false
         private set
-        get() = b && !bPrev
+        get() = bCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var xToggle = false
         private set
-        get() = x && !xPrev
+        get() = xCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var yToggle = false
         private set
-        get() = y && !yPrev
+        get() = yCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
 
     var leftBumperToggle = false
         private set
-        get() = leftBumper && !leftBumperPrev
+        get() = leftBumperCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var rightBumperToggle = false
         private set
-        get() = rightBumper && !rightBumperPrev
+        get() = rightBumperCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
 
     var dpadUpToggle = false
         private set
-        get() = dpadUp && !dpadUpPrev
+        get() = dpadUpCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var dpadDownToggle = false
         private set
-        get() = dpadDown && !dpadDownPrev
+        get() = dpadDownCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var dpadLeftToggle = false
         private set
-        get() = dpadLeft && !dpadLeftPrev
+        get() = dpadLeftCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
     var dpadRightToggle = false
         private set
-        get() = dpadRight && !dpadRightPrev
+        get() = dpadRightCountOn == DEBOUNCING_NUMBER_OF_SAMPLES
 
     fun update() {
+        if(a) aCountOn++
+        else aCountOn = 0
+        if(b) bCountOn++
+        else bCountOn = 0
+        if(x) xCountOn++
+        else xCountOn = 0
+        if(y) yCountOn++
+        else yCountOn = 0
+
+        if(leftBumper) leftBumperCountOn++
+        else leftBumperCountOn = 0
+        if(rightBumper) rightBumperCountOn++
+        else rightBumperCountOn = 0
+
+        if(dpadUp) dpadUpCountOn++
+        else dpadUpCountOn = 0
+        if(dpadDown) dpadDownCountOn++
+        else dpadDownCountOn = 0
+        if(dpadLeft) dpadLeftCountOn++
+        else dpadLeftCountOn = 0
+        if(dpadRight) dpadRightCountOn++
+        else dpadRightCountOn = 0
+        
+        
+        /*
         aPrev = a
         bPrev = b
         xPrev = x
@@ -113,5 +150,6 @@ class Controller(private val gamepad: Gamepad) {
         dpadDownPrev = dpadDown
         dpadLeftPrev = dpadLeft
         dpadRightPrev = dpadRight
+        */
     }
 }
